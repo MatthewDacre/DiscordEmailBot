7ee85bfb233fbf13a294dd8a22613d2bea10af60
Matthew2091295 <2091295@students.wits.ac.za>
2021-07-11 12:53:15 +0200

    Adding Discord messaging functionality

--- emailPull.py
+++ emailPull.py
@@ -3,11 +3,12 @@ import smtplib
 import time
 import imaplib
 import email
-import traceback 
-
-ORG_EMAIL = "@students.wits.ac.za" 
-FROM_EMAIL = "2091295" + ORG_EMAIL 
-FROM_PWD = "Hair121314" 
+import traceback
+import discord
+import os 
+ 
+FROM_EMAIL =  os.getenv("EMAIL")
+FROM_PWD = os.getenv("PASS")
 SMTP_SERVER = "imap.gmail.com" 
 SMTP_PORT = 993
 
@@ -32,32 +33,40 @@ def read_email_from_gmail(n):
                     msg = email.message_from_string(str(arr[1],'utf-8'))
                     email_subject = msg['subject']
                     email_from = msg['from']
-                    ret.append( email_from) 
+                    ret.append(email_from) 
                     ret.append(email_subject)
                     ret.append(i)
-
         return ret
 
     except Exception as e:
         traceback.print_exc() 
         print(str(e))
 
-latestEmail = 0
+client = discord.Client()
 
-emails = read_email_from_gmail(1)
-firstIter = False
-latestEmail = emails[2]
-print(emails)
-now = datetime.datetime.now()
-oldTime = now.hour*60 + now.minute
-i = 0
-while True:
+@client.event
+async def on_ready():
     now = datetime.datetime.now()
-    currTime = now.hour*60 + now.minute
-    if abs(currTime - oldTime) >= 5:
-        i += 1
-        oldTime = currTime
-        emails = read_email_from_gmail(1)
-        if(emails[2] != latestEmail):
-            latestEmail = emails[2]
-            print(emails)
+    channel = client.get_channel(863720376340447234)
+    emails = read_email_from_gmail(5)
+    print("Fetched Emails: {}".format(emails))
+    latestEmail = emails[2]
+    msg = "="*50 + "\n@here\nLast 5 Emails:\n" + "From: {}\nSubject: {}\n\nFrom: {}\nSubject: {}\n\nFrom: {}\nSubject: {}\n\nFrom: {}\nSubject: {}\n\nFrom: {}\nSubject: {}\n".format(emails[0], emails[1], emails[3], emails[4], emails[6], emails[7], emails[9], emails[10], emails[12], emails[13])
+    await channel.send(msg)
+
+    while True:
+        delta = abs((now.now() - now).total_seconds())
+        print(delta)
+        if delta >= 50:
+            print("Entered Loop")
+            now = now.now()
+            emails = read_email_from_gmail(1)
+            print("Last Email ID: {}\nCurr Email ID: {}".format(latestEmail, emails[2]))
+            if(emails[2] != latestEmail):
+                latestEmail = emails[2]
+                print("Fetched Emails: {}".format(emails))
+                msg = "="*50 + "\n@here\nNew Email:\n" + "From: {}\nSubject: {}".format(emails[0], emails[1])
+                await channel.send(msg)
+
+
+client.run(os.getenv("TOKEN"))
